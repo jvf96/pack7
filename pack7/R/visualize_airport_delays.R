0c885ea8cf6462a14436e251128bb46b9f9d4b72
jvf96 <31595919+jvf96@users.noreply.github.com>
2017-11-18 04:53:13 +0100

    final commit4

--- pack7/R/visualize_airport_delays.R
+++ pack7/R/visualize_airport_delays.R
@@ -2,6 +2,8 @@
 #' @description Creates a plot that visualizes the mean delay of ﬂights for diﬀerent airports by longitude and latitude.
 #' @return a plot
 #' 
+#' @importFrom stats na.omit
+#' @importFrom methods new
 #' @export
 #' 
 
@@ -15,10 +17,10 @@ visualize_airport_delays <- function(){
   
   data_airports <- dplyr::inner_join(airports, data_flights, by = c("faa" = "dest"))
   
-  ggplot2::ggplot(data_airports, aes(y = data_airports$lat, x = data_airports$lon)) + 
+  ggplot2::ggplot(data_airports, ggplot2::aes(y = data_airports$lat, x = data_airports$lon)) + 
     ggplot2::geom_point(na.rm =TRUE) + ggplot2::theme_gray() + 
     ggplot2::scale_color_gradient(low = "black", high = "#F5F5F5") + 
     ggplot2::labs(title = "Average arrival delays", subtitle = "Longitude vs. Latitude",
                   y = "Latitude", x = "Longitude", color = "Arrival delays") + 
-    ggplot2::theme(plot.title = element_text(hjust = 0.5))
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
 }
